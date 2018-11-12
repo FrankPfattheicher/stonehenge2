fcaa481d6d5a7a935935c57b809d95d10a3137fc
Frank Pfattheicher <fpf@ict-baden.de>
2018-11-12 22:44:42 +0100

    Switched back to .NET Framework 4.5.1 to be Mono compatible.

--- Stonehenge2-AssemblyInfo.cs
+++ Stonehenge2-AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 [assembly: AssemblyCopyrightAttribute("Copyright ©2013-2018 ICT Baden GmbH")]
 [assembly: AssemblyCompanyAttribute("ICT Baden GmbH")]
 [assembly: AssemblyProductAttribute("Web Application Framework")]
-[assembly: AssemblyVersionAttribute("2.2.11")]
-[assembly: AssemblyFileVersionAttribute("2.2.11")]
+[assembly: AssemblyVersionAttribute("2.2.12")]
+[assembly: AssemblyFileVersionAttribute("2.2.12")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyCopyright = "Copyright ©2013-2018 ICT Baden GmbH";
         internal const System.String AssemblyCompany = "ICT Baden GmbH";
         internal const System.String AssemblyProduct = "Web Application Framework";
-        internal const System.String AssemblyVersion = "2.2.11";
-        internal const System.String AssemblyFileVersion = "2.2.11";
+        internal const System.String AssemblyVersion = "2.2.12";
+        internal const System.String AssemblyFileVersion = "2.2.12";
     }
 }
